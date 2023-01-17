8e2bd192f75df6434e95030f275da750f5e25caf
Helton Heleno <helton_heleno@hotmail.com>
2023-01-17 17:23:17 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app =  Fastify()
+const prisma = new PrismaClient()
+
+/*
+    * Metodo HTTP: Get, Post, Patch, Delete
+*/
+app.register(cors)
+
+app.get('/', async () => {
+    const habits = await prisma.habit.findMany()
+
+
+    return habits
+})
+
+app.listen({
+    port:3333,
+}).then(() => {
+    console.log('HTTP Server running!');
+    
+})
